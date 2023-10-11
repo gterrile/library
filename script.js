ba6889fa2faded28440c05a6b4be22b435264bdc
German Terrile <gterrile@gmail.com>
2023-10-10 18:15:41 -0700

    fixed style and layout. Need to fix read toogle to update library database

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ function displayForm() {
 }
 
 // Book Object Constructor
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -32,8 +33,18 @@ function Book(title, author, pages, read) {
 }
 
 // Add to Library
-function addToLibrary(event) {
-  let newBook = new Book(title.value, author.value, pages.value, read.value)
+function addToLibrary() {
+  
+  // Generate book id
+  const newId = myLibrary.length;
+
+  // Generate book read status 
+  let readFlag = false;
+  if (read.checked) {
+    readFlag = true;
+  }
+
+  let newBook = new Book(newId, title.value, author.value, pages.value, readFlag)
   myLibrary.push(newBook);
 
   // Display book
@@ -44,22 +55,63 @@ function addToLibrary(event) {
   const bookTitle = document.createElement('span');
   const bookAuthor = document.createElement('span');
   const bookPages = document.createElement('span');
-  const bookRead = document.createElement('span');
   
+  //const bookRead = document.createElement('span');
+  const bookLabel = document.createElement('label');
+  bookLabel.classList.add('switch');
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.setAttribute('id', `read_${newId}`);
+  checkBox.setAttribute('name', 'read');
+  if (read.checked) {
+    checkBox.checked = true;
+  }
+  else {
+    checkBox.checked = false;
+  }
+  const readSpan = document.createElement('span');
+  readSpan.classList.add('slider','round');  
+  bookLabel.appendChild(checkBox);
+  bookLabel.appendChild(readSpan);
+
+  const bookDelete = document.createElement('button');
+  const deleteSpan = document.createElement('span');
+  deleteSpan.classList.add('material-symbols-outlined');
+  bookDelete.appendChild(deleteSpan);
+  deleteSpan.textContent = 'delete_forever'
+  deleteSpan.setAttribute('style', 'font-size: 32px; color:tomato');
+
   bookTitle.textContent = title.value;
   bookAuthor.textContent = author.value;
   bookPages.textContent = pages.value;
-  bookRead.textContent = read.value;
+
 
   bookTitle.classList.add('book-field', 'title');
   bookAuthor.classList.add('book-field');
   bookPages.classList.add('book-field');
-  bookRead.classList.add('book-field');
+  bookLabel.classList.add('book-field');
+  bookDelete.classList.add('book-field', 'delete');
+  bookDelete.setAttribute('id', newId);
+
+  // Delete button function
+  bookDelete.onclick = function() {
+
+    //Remove from myLibrary
+    const removeId = bookDelete.getAttribute('id');
+    myLibrary.splice(removeId, 1);
+
+    // Remove from display
+    while (bookContainer.firstChild) {
+      bookContainer.removeChild(bookContainer.firstChild);
+    }
+    libraryContent.removeChild(bookContainer);
+  }
 
   bookContainer.appendChild(bookTitle);
   bookContainer.appendChild(bookAuthor);
   bookContainer.appendChild(bookPages);
-  bookContainer.appendChild(bookRead);
+  bookContainer.appendChild(bookLabel);
+  bookContainer.appendChild(bookDelete);
   
   // Reset page display
   formSection.style.display = 'none';
@@ -67,11 +119,3 @@ function addToLibrary(event) {
 
 }
 
-// read.addEventListener('change', function() {
-//   if (read.checked == true) {
-//     console.log('checked');
-//   }
-//   else {
-//     console.log('unchecked');
-//   }
-// })
